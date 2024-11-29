3c70f2dcabae0e91edfc94abe11329bb4d3dfc92
PopovicJovan <jovanp.school06@gmail.com>
2024-11-29 12:08:45 +0100

    all origins are allowed

--- app/main.py
+++ app/main.py
@@ -4,9 +4,22 @@ from fastapi import FastAPI
 from app.routers.auth import router as r1
 from app.routers.user import router as r2
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
-
 app = FastAPI()
+
+origins = [
+# Custom origins
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(r1)
 app.include_router(r2)
