7a1798c2d44d1670fdba57dcf85787518d0f356e
PopovicJovan <jovanp.school06@gmail.com>
2024-12-01 12:13:05 +0100

    admin field in user model added

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,6 @@
 from datetime import datetime
-
-from sqlalchemy import Integer, String, DateTime, func
+from sqlalchemy import Integer, String, DateTime, func, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
-
 from app.database.database import Base
 
 
@@ -13,4 +11,5 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
     email: Mapped[str] = mapped_column(String(64), unique=True)
     password: Mapped[str] = mapped_column(String(256))
+    admin: Mapped[bool] = mapped_column(Boolean, default=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now())
