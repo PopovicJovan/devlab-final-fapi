09a00f20392abf6a5d1ae79fa6d67dd3d63fa777
PopovicJovan <jovanp.school06@gmail.com>
2024-12-13 16:51:27 +0100

    yachts table added

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 from app.models.status import Status
 from app.models.model import Model
+from app.models.yacht import Yacht
 
-__all__ = ["User", "Status", "Model"]
+__all__ = ["User", "Status", "Model", "Yacht"]
---
+++ app/models/yacht.py
@@ -0,0 +1,26 @@
+from datetime import datetime, date
+from sqlalchemy import Integer, String, DateTime, func, Boolean, Date, Float, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database.database import Base
+
+
+class Yacht(Base):
+    __tablename__ = 'yachts'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    model_id: Mapped[int] = mapped_column(Integer, ForeignKey('models.id'))
+    status_id: Mapped[int] = mapped_column(Integer, ForeignKey('statuses.id'))
+
+    name: Mapped[str] = mapped_column(String(32))
+    description: Mapped[str] = mapped_column(Text)
+    year: Mapped[date] = mapped_column(Date)
+
+    sale_price: Mapped[float] = mapped_column(Float)
+    rent_price: Mapped[float] = mapped_column(Float)
+
+    picture: Mapped[str] = mapped_column(String(256), nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now())
+
+    model: Mapped["Model"] = relationship("Model")
+    status: Mapped["Status"] = relationship("Status")
